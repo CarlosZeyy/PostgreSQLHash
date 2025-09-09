6e30e71552c27eca93bc029d814d71300e00a85a
Carlos Moises <carloszeeyy@gmail.com>
2025-09-09 20:35:40 -0300

    Add: Conteudo do modulo 1 de PostgreSQL da Hashtag treinamentos

---
+++ 48. PostgreSQL - Criando queries básicas no Postgres/Aula1- FROM - Selecionando Dados da tabela.sql
@@ -0,0 +1,15 @@
+SELECT * FROM products;
+
+SELECT product_id, product_name FROM products;
+
+SELECT 
+	product_id,
+	product_name,
+	unit_price,
+	units_in_stock
+FROM products; -- Se não tiver um ';' fechando o codigo e logo abaixo eu tiver outro codigo vai dar erro.
+
+
+-- SELECT 
+-- 	employee_id, first_name, last_name, city
+-- FROM employees;
